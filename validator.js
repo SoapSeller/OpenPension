03479733d5943141f0c8f0458070444115e367fb
Lior Paz <lior.paz@prezi.com>
2013-07-08 21:45:18 +0300

    remove empty lines

--- validator.js
+++ validator.js
@@ -1,7 +1,9 @@
+var MetaTable = require('./MetaTable')
+
 exports.validate = function(supplier, tabIndex, headers, content) {
 // supplier; (string) 'Migdal' לדגמ
 // tabIndex; (integer) the tab number in the suppliers xls sheet
-// headers; (array of objects) [{columnName: 'instrument id'}] 
+// headers; (array of objects) [{columnName: 'instrument_id'}] 
 //  
 // content; (array of arrays - each of which has the same length as headers) [
 // 				["instrument 1"],
@@ -20,18 +22,44 @@ exports.validate = function(supplier, tabIndex, headers, content) {
 		}
 	}
 
-	// for each content array,
-	// 	validate all values are not null, not empty strings, (not 0?)
-	// 	for each column
-	// 		validate the column data type matches its value
+	removeEmptyLines(content)
+	removeSumLines(content)
+	validateTypes();
 
 
 	var db = require('./db').open();
 	var tableWriter = db.openTable(headers)
 	tableWriter(content);
 
+	console.log("<><><<>< headers", headers)
 	console.log("<><><<>< Sheet Data",content);
 	console.log("<><><<>< Validation", validStatus);
-
 }
 
+function removeEmptyLines(content){
+	for (var i = content.length - 1 ; i > -1; i--){
+		if (lineIsEmpty(content[i])){
+			content.splice(i, 1);
+		}
+	}
+}
+
+function lineIsEmpty(line)
+{
+	for (var i = 0; i < line.length; i++)
+	{
+		if (line[i] != '')
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+function removeSumLines(content){
+	
+}
+
+function validateTypes(){
+	
+}
