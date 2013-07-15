06ee35f2dc76142693a90cd2c9cd911a2b2b1338
romansky <roman.land@gmail.com>
2013-07-15 22:24:34 +0300

    +patching validator to do CSV

--- validator.js
+++ validator.js
@@ -1,6 +1,6 @@
 var MetaTable = require('./MetaTable')
 
-exports.validate = function(supplier, tabIndex, headers, content) {
+exports.validate = function(supplier, tabIndex, headers, content,tabIndex) {
 // supplier; (string) 'Migdal' לדגמ
 // tabIndex; (integer) the tab number in the suppliers xls sheet
 // headers; (array of objects) [{columnName: 'instrument_id'}] 
@@ -17,13 +17,13 @@ exports.validate = function(supplier, tabIndex, headers, content) {
 	removeSumLines(content)
 	validateTypes();
 
-
-	var db = require('./db').open();
+	var DB =  require('./db');
+	var db = new DB.csv(supplier + "_tab_" + tabIndex + ".csv");
 	var tableWriter = db.openTable(headers)
 	tableWriter(content);
 
-	console.log("<><><<>< headers", headers)
-	console.log("<><><<>< Sheet Data",content);
+	// console.log("<><><<>< headers", headers)
+	// console.log("<><><<>< Sheet Data",content);
 }
 
 function removeBadLengthLines(content, numColumns)
@@ -31,7 +31,7 @@ function removeBadLengthLines(content, numColumns)
 	for (var i = content.length - 1 ; i > -1; i--){
 		if (content[i].length !== numColumns){
 			content.splice(i, 1);
-			console.log ('line ' + i + ' has ' + content[i].length + ' columns. expected ' + numColumns + '.');
+			// console.log ('line ' + i + ' has ' + content[i].length + ' columns. expected ' + numColumns + '.');
 		}
 	}
 }
