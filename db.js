8025f226d91deb4636fef10f836f4b67b422958f
Dor Shahaf <dor@shahaf.com>
2013-08-05 22:09:50 +0300

    Fix initial GRANT SELECT for opublic after creating the table.
    + Move table name to variable.

--- db.js
+++ db.js
@@ -17,11 +17,12 @@ var defaultColumnsNamesMapping = defaultColumnnNames.map(function(c) { return { 
 
 var config;
 try {
-  config = require("./_config")
+  config = require("./_config");
 } catch (ignore){
-  config = require('./config') 
+  config = require('./config');
 }
 
+var tableName = 'data';
 
 var db = {};
 
@@ -52,11 +53,10 @@ db.csv.prototype = {
       }
       indexes.push(idx);
     });
-    console.log(_.zip(columnsNames, indexes));
+
     return function(managing_body, report_year, report_qurater, instrument_type, instrument_sub_type, objects) {
       objects.forEach(function(object) {
         object = [managing_body, report_year.toString(), report_qurater.toString(), instrument_type, instrument_sub_type].concat(object);
-        console.log(object);
 
         for (i = 0; i < indexes.length; ++i) {
           var idx = indexes[i];
@@ -94,19 +94,27 @@ var mapColumnType2Sql = function(type) {
 };
 
 db.pg = function() {
-  this.client = new pg.Client(config.connection_string);
-  this.client.connect();
+  var self = this;
 
-  this.tablesCounter = 0;
+  self.client = new pg.Client(config.connection_string);
+  self.client.connect();
 
-  var createTable = "CREATE TABLE data(id BIGSERIAL PRIMARY KEY, ";
+  self.tablesCounter = 0;
+
+  var createTable = "CREATE TABLE " + tableName + "(id BIGSERIAL PRIMARY KEY, ";
   var fields = _.zip(columnsNames, columnsTypes.map(mapColumnType2Sql));
   createTable += fields.filter(function(f) { return !!f[0] && !!f[1]; }).map(function(f) { return f[0] + " " + f[1]; }).join(',');
   createTable += ");";
 
-  this.client.query(createTable, function(err) {
+  self.client.query(createTable, function(err) {
     if(!err) {
-      this.client.query('GRANT SELECT ON data TO opublic;');
+      self.client.query('GRANT SELECT ON ' + tableName + ' TO opublic;', function(err) {
+        if (err) {
+          console.log("error in grant", err);
+        }
+      });
+    } else {
+      console.log("error in create", err);
     }
   });
 };
@@ -130,7 +138,7 @@ db.pg.prototype = {
       }
     });
 
-    var sql = "INSERT INTO data(" + mapping.map(function(m) { return m.columnName; }).join(',') + ")  " +
+    var sql = "INSERT INTO " + tableName + "(" + mapping.map(function(m) { return m.columnName; }).join(',') + ")  " +
                    "VALUES (" + _.range(mapping.length).map(function(n) { return "$" + (n+1);}) + ");";
 
     var statment = { name: name, text: sql, values: null };
