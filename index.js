c2aae0483313e661306818b0e01e3cde8a2d069b
Shakhal Levinson <shakhalevinson@gmail.com>
2015-11-30 06:32:59 +0200

    fix commander syntax

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var initialize = require('./initialize');
 
 //creates system dirs on first run
 program
-	.command("init", "create system directories")
+	.command("init")
+	.description("create system directories")
 	.action(function(args){
 		require('./initialize').init()
 		.then(function(){
@@ -20,7 +21,8 @@ program
 
 //clean system dirs (deletes files!)
 program
-	.command("clean", "clean (delete files) in system directories")
+	.command("clean")
+	.description("clean (delete files) in system directories")
 	.action(function(args){
 		require('./initialize').clean()
 		.then(function(){
@@ -30,59 +32,66 @@ program
 
 //convert excel file(s) in directory to csv
 program
-	.command("convert-files", "convert excel files to csv")
-	.option("-d, --dir <name>","directory name")
+	.command("convert-files")
+	.description("convert excel files to csv")
+	.option("-d, --dir <name>","directory name", null, 'a')
 	.option("-y, --year <year>", "year")
 	.option("-q, --quarter <quarter>", "quarter")
 	.option("-b, --body <body>", "body")
 	.option("-m, --monkey <monkey>", "monkey")
-	.action(function(args){
+	.action(function(env, args){
 		require("./files_loader").convertFiles(args.dir, args.body, args.monkey, args.year, args.quarter);
 	})
 
 //create table in database
 program
-  .command("db-create-table", "create table in database")
+  .command("db-create-table")
+  .description("create table in database")
   .option("-t, --table <name>","table name")
-  .action(function(args){
+  .action(function(env, args){
     require('./db').createTable(args.table);
   });
 
 //truncate table in database
 program
-  .command("db-empty-table", "truncate table in database")  
+  .command("db-empty-table")
+  .description("truncate table in database")  
   .option("-t, --table <name>","table name")
-  .action(function(args){
+  .action(function(env, args){
     require('./db').emptyTable(args.table);
   });
 
 program
-	.command("dump-funds", "create csv files from database data")
+	.command("dump-funds")
+	.description("create csv files from database data")
 	.action(function(){
 		require('./fetcher').dumpFunds();
 	});
 
 //download and convert files in Google Doc
 program
-	.command("fetch-known", "download and convert files in Google Doc")
+	.command("fetch-known")
+	.description("download and convert files in Google Doc")
 	.option("-y, --year <year>", "year")
 	.option("-q, --quarter <quarter>", "quarter")
 	.option("-b, --body <body>", "body")
 	.option("-m, --monkey <monkey>", "monkey")
-	.action(function(args){
+	.action(function(env, args){
 		require('./fetcher').fetchKnown(args.body, args.year, args.quarter, args.monkey);
 	});
 
 //download and convert contributed files
 program
-    .command("fetch-contrib", "download and convert contributed files")
+    .command("fetch-contrib")
+    .description("download and convert contributed files")
     .action(function(){
         require('./fetcher').fetchContrib();
     });
 
 //load files to database
 program
-	.command("db-load-files", "load csv files to database")
+	.command("db-load-files")
+	.description("load csv files to database")
 	.option("-d, --dir <name>","directory name")
 	.option("-y, --year <year>", "year")
 	.option("-q, --quarter <quarter>", "quarter")
@@ -90,7 +99,7 @@ program
 	.option("-f, --fund <fund number>", "fund number")
 	.option("-t, --table <name>","table name")
 	.option("-c, --concurrency <number>","number of concurrent DB connections, defaults to 4")
-	.action(function(args){
+	.action(function(env, args){
 		require('./dbLoader').importFilesCmd(args.dir, args.body, args.year, args.quarter, args.fund, 
 			args.table, args.concurrency);
 	})
