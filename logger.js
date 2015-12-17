a827fd25cff4d99664444464ef2bd77d34c77d04
Shakhal Levinson <shakhalevinson@gmail.com>
2015-12-17 17:56:52 +0200

    log module file name

--- logger.js
+++ logger.js
@@ -1,19 +1,44 @@
 var winston = require('winston');
+var path = require('path');
 
-module.exports = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)(),
-    new (winston.transports.File)({
-      name: 'info-file',
-      filename: 'info.log',
-      level: 'info'
-    }),
-    new (winston.transports.File)({
-      name: 'error-file',
-      filename: 'error.log',
-      level: 'error'
-    })
-  ]
-});
+module.exports = function(module){
+  var moduleFileName = path.basename(module.filename);
+
+  return new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+            level: 'info', //debug
+            formatter: function(log) {
+              return "["+ log.level.toUpperCase() +"] "+moduleFileName+": "+ log.message;
+            }
+          }
+      ),
+      new (winston.transports.File)({
+        name: 'debug-file',
+        filename: 'debug.log',
+        json: false,
+        level: 'debug',
+        formatter: function(log) {
+          return "["+ log.level.toUpperCase() +"] "+moduleFileName+": "+ log.message;
+        }
+      }),
+      new (winston.transports.File)({
+        name: 'error-file',
+        json: false,
+        filename: 'error.log',
+        level: 'warn',
+        formatter: function(log) {
+          return "["+ log.level.toUpperCase() +"] "+moduleFileName+": "+ log.message;
+        }
+      })
+    ],
+    levels: {
+      error: 4,
+      warn: 3,
+      info: 2,
+      debug: 1
+    }
+  });
+};
 
 
